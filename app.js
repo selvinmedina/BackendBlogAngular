1e53817f1293971637ddc6d31f048795a7ed6ae3
selvinmedina <selvinmedina0016@gmail.com>
2020-03-26 02:47:55 -0600

    cors

--- app.js
+++ app.js
@@ -16,13 +16,7 @@ app.use(bodyParser.json());
 
 // Cors
 // Configurar cabeceras y cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+
 
 // Añadir prefijos a rutas
 app.use('/api', articleRoutes);
